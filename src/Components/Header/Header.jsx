ef9b7c1a3093680dfa2129a3598ee1dccb44903e
DhruTesh <dhruteshdev5@gmail.com>
2024-12-05 13:32:30 +0530

    test

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,16 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom'; 
+import LottieAnimation from '../LottieAnimation'; 
+
 const Header = () => {
+  const [showAnimation, setShowAnimation] = useState(false); 
+
+  const handleLogoClick = () => {
+    setShowAnimation(true); 
+  };
+
+  const handleAnimationComplete = () => {
+    setShowAnimation(false); 
+  };
+
   return (
-    <header className="flex justify-between items-center p-4 bg-gray-800 text-white">
+    <header className="flex flex-col items-center p-4 bg-gray-800 text-white">
       <div className="flex items-center">
         <img 
-          src="https://static.vecteezy.com/system/resources/previews/008/296/267/non_2x/colorful-swirl-logo-design-concept-illustration-vector.jpg" 
+          src="https://static.vecteezy .com/system/resources/previews/008/296/267/non_2x/colorful-swirl-logo-design-concept-illustration-vector.jpg" 
           alt="Logo" 
-          className="h-20 w-20 mr-2" 
+          className="h-20 w-20 mr-2 cursor-pointer" 
+          onClick={handleLogoClick} // Add onClick event to the logo
         />
         <h1 className="text-2xl font-bold">FusionBazaar</h1>
       </div>
+      {showAnimation && <LottieAnimation onAnimationComplete={handleAnimationComplete} />} 
       <nav className="flex space-x-4">
         <Link to="/" className="hover:text-gray-400 text-xl">Home</Link> 
         <Link to="/aboutus" className="hover:text-gray-400 text-xl ">AboutUs</Link>
@@ -22,7 +36,6 @@ const Header = () => {
             Login
           </button>
         </Link>
-        
       </div>
     </header>
   );
